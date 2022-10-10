f65ca917ba0e28d3e12cdbd84df9f7c9e62b1dba
smforuzmand <career.forouzmand@gmail.com>
2022-10-10 23:43:49 +0200

    initial commit

--- src/main/java/se/lexicon/springMVCThymeleaf/controller/Home.java
+++ src/main/java/se/lexicon/springMVCThymeleaf/controller/Home.java
@@ -2,22 +2,46 @@ package se.lexicon.springMVCThymeleaf.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping("/Home")
 public class Home {
 
 
-    @GetMapping(path = "/index")
-    public String index(){
+    List<String> strings;
 
-        return "index";
+
+
+    @GetMapping(path = "/index")
+    public String index() {
+        return "indexView";
+    }
+
+    @GetMapping("/custom")
+    public String contactList(Model theModel) {
+        strings = new ArrayList<>();
+        theModel.addAttribute("stringList", strings);
+        return "stringView";
 
     }
 
 
 
+    @PostMapping ("/contact")
+    public String contact(@RequestParam("name") String name , Model model) {
 
+        System.out.println("name = " + name);
+        model.addAttribute("contactView", strings);
 
+        return "contactView";
+    }
 
 }
